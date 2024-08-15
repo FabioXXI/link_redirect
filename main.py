2d9d5f5cf6ef853a0ddc0715782fee5929c4ba36
ninja <astore.a.fabio@gmail.com>
2024-08-14 21:27:45 -0300

    "link redirect"

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import routers.url
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(routers.url.router)
